c491cea97abdf18130077dcd6945a0eae72ee5ab
cydneysmith <cydney.smith@kellogg.northwestern.edu>
2022-06-03 21:27:44 +0000

    end of class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    respond_to do |format|
+        format.html
+        format. json do
+          render :json => @posts
+        end
+    end
   end
 
   def new
@@ -12,7 +18,7 @@ class PostsController < ApplicationController
     if @current_user
       @post = Post.new
       @post["body"] = params["post"]["body"]
-      @post["image"] = params["post"]["image"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
       @post["user_id"] = @current_user["id"]
       @post.save
     else
